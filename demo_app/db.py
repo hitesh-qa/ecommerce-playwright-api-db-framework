8452d12880a2d1a99ebb26e6af9e3e80030e51d9
hitesh-qa <hitesh33733@gmail.com>
2026-08-04 08:37:57 +0530

    working on get user and create order in db.py

--- demo_app/db.py
+++ demo_app/db.py
@@ -20,3 +20,31 @@ def get_product(product_id):
     conn.close()
     return dict(row) if row else None
 
+def create_user(username,email,password):
+    conn = get_connection()
+    cursor = conn.execute(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        (username, email, password)
+    )
+    conn.commit()
+    user_id = cursor.lastrowid
+    conn.close()
+    return user_id
+
+def get_user_by_username(username):
+    conn = get_connection()
+    row = conn.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone()
+    conn.close()
+    return dict(row) if row else None
+
+def create_order(user_id, product_id, quantity, total_amount):
+    conn = get_connection()
+    cursor = conn.execute(
+        "INSERT INTO orders (user_id, product_id, quantity, total_amount) VALUES (?, ?, ?, ?)",
+        (user_id, product_id, quantity, total_amount)
+    )
+    conn.commit()
+    order_id = cursor.lastrowid
+    conn.close()
+    return order_id
+
